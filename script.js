c144763b4777a6a3789c3405fe8e039b7d82c430
ana-novak <novak.anastasiya.v@gmail.com>
2025-01-07 22:34:11 +0400

    Fix form

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('rsvpForm').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent default form submission
+  e.preventDefault();
 
   const form = e.target;
   const formData = new FormData(form);
@@ -7,17 +7,17 @@ document.getElementById('rsvpForm').addEventListener('submit', function(e) {
   fetch('https://docs.google.com/forms/d/e/1FAIpQLSfmWGGHjoV4tnFceQa8n_AcH4JL6NBo8lsP9S2m7klB3lJAng/formResponse', {
     method: 'POST',
     body: formData,
-    mode: 'no-cors' // Prevent CORS errors
+    mode: 'no-cors'
   }).then(() => {
     const message = document.getElementById("responseMessage");
     message.classList.remove("hidden");
     message.classList.add("visible");
-    // Через 3 секунды скрываем сообщение
+    document.getElementById("submitButton").disabled = true;
     setTimeout(() => {
       message.classList.remove("visible");
       message.classList.add("hidden");
     }, 3000);
-    form.reset(); // Reset the form
+    form.reset();
   }).catch(() => {
     alert('There was an error submitting your RSVP. Please try again later.');
   });
